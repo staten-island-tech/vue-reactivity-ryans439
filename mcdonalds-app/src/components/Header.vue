19c2e8044b464032e83f79452fe0b25c77968f01
ryans439 <ryans439@nycstudents.net>
2025-02-05 13:38:43 -0500

    jjk

---
+++ mcdonalds-app/src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+    <header>
+      <nav>
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/menu">Menu</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+          <li><router-link to="/contact">Contact</router-link></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Header'
+  };
+  </script>
+  
+  <style>
+  header {
+    background-color: #ffcc00;
+    padding: 20px;
+  }
+  
+  nav ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+  }
+  
+  nav ul li {
+    margin: 0 15px;
+  }
+  
+  nav ul li a {
+    text-decoration: none;
+    color: #333;
+    font-weight: bold;
+  }
+  
+  nav ul li a:hover {
+    color: #000;
+  }
+  </style>
+  
